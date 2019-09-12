be0cfafc65906967453b7caaac34c37b1f7303cc
diyaguang <diyaguang@139.com>
2019-09-12 09:13:16 +0800

    Revert "update"
    This reverts commit 90eecba6440811faca89ac7678af9bde5b81eeed.

--- book_service/models/movie.js
+++ book_service/models/movie.js
@@ -3,19 +3,19 @@ var movie = new mongoose.Schema({
     movieName: String,
     movieImg: String,
     movieVideo: String,
-    movieDownload: String,
+    movieDownload: Number,
     movieTime: String,
     movieNumSuppose: Number,
     movieNumDownload: Number,
     movieMainPage: Boolean
 })
 
-movie.statics.findById = function(id,callBack){
-    this.findOne({_id:id},callBack);
+var movieModel = mongoose.model('movie',movie);
+
+movie.statics.findById = function (m_id,callBack) {
+    this.find({movie_id:m_id,check:true},callBack);
 };
-movie.statics.findAll = function(callBack){
+movie.statics.findAll = function (callBack) {
     this.find({},callBack);
 };
-
-var movieModel = mongoose.model('movie',movie);
 module.exports = movieModel;
--- book_service/routes/index.js
+++ book_service/routes/index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-
-var recommend = require('../models/recommend');
+var recommend = require('../models/comment');
 var movie = require('../models/movie');
 var article = require('../models/article');
 var user = require('../models/user');
@@ -11,6 +10,7 @@ var user = require('../models/user');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 router.get('/mongooseTest',function (req,res,next) {
   mongoose.connect('mongodb://root:96777@39.105.102.17:27017/TestVue',{useNewUrlParser: true});
   mongoose.Promise = global.Promise;
@@ -25,45 +25,6 @@ router.get('/mongooseTest',function (req,res,next) {
     }
   });
   res.send('数据库链接测试页面')
-});
-
-
-router.get('/showIndex',function (req,res,next) {
-  recommend.findAll(function (err,getRecommend) {
-    res.json({status:0,message:"获取推荐",data:getRecommend})
-  })
-});
-router.get('/showRanking',function (req,res,next) {
-  movie.find({movieMainPage:true},function (err,getMovies) {
-    res.json({status:0,message:"获取主页",data:getMovies});
-  })
-});
-router.get('/showArticle',function (req,res,next) {
-  article.findAll(function (err,getArticles) {
-    res.json({status:0,message:"获取主页",data:getArticles});
-  })
-});
-router.post('/articleDetail',function (req,res,next) {
-  if(!req.body.article_id){
-    res.json({status:1,message:"文章ID出错"});
-  }
-  article.findByArticleId(req.body.article_id,function (err,getArticle) {
-    res.json({status:0,message:"获取成功",data:getArticle});
-  })
-});
-router.post('/showUser',function (req,res,next) {
-  if(!req.body.user_id){
-    res.json({status:1,message:"用户状态出错"})
-  }
-  user.findById(req.body.user_id,function (err,getUser) {
-    res.json({status:0,message:"获取成功",data:{
-        user_id: getUser._id,
-        username: getUser.username,
-        userMail: getUser.userMail,
-        userPhone: getUser.userPhone,
-        userStop: getUser.userStop
-      }})
-  })
 });
 
 module.exports = router;
