90eecba6440811faca89ac7678af9bde5b81eeed
diyaguang <diyaguang@139.com>
2019-09-12 08:59:19 +0800

    update

--- book_service/models/movie.js
+++ book_service/models/movie.js
@@ -3,19 +3,19 @@ var movie = new mongoose.Schema({
     movieName: String,
     movieImg: String,
     movieVideo: String,
-    movieDownload: Number,
+    movieDownload: String,
     movieTime: String,
     movieNumSuppose: Number,
     movieNumDownload: Number,
     movieMainPage: Boolean
 })
 
-var movieModel = mongoose.model('movie',movie);
-
-movie.statics.findById = function (m_id,callBack) {
-    this.find({movie_id:m_id,check:true},callBack);
+movie.statics.findById = function(id,callBack){
+    this.findOne({_id:id},callBack);
 };
-movie.statics.findAll = function (callBack) {
+movie.statics.findAll = function(callBack){
     this.find({},callBack);
 };
+
+var movieModel = mongoose.model('movie',movie);
 module.exports = movieModel;
