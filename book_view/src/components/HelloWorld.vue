1a90106aee087b0d2a80c9f8dc7d59ce897f7e94
diyaguang <diyaguang@139.com>
2019-09-17 17:53:47 +0800

    add test code

--- book_view/src/components/HelloWorld.vue
+++ book_view/src/components/HelloWorld.vue
@@ -1,85 +1,16 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <span v-pre>{{ name }}</span> <br>
+    <span v-html="link"></span> <br>
+    <p v-if="show">这是一段文本</p><br>
+    <button v-on:click="handleClose">点击隐藏</button><br>
+    <div>
+      {{ number /10 }} <br>
+      {{ text.split(',').reverse().join(',') }} <br>
+      {{ isOK?'确定':'取消'}} <br>
+      {{ date | formatDate }}
+    </div>
   </div>
 </template>
 
@@ -88,9 +19,46 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'This is dygstuio page',
+      link: '<a href="#">这是一个链接</a>',
+      number: 1983,
+      text: 'This,my,first,vue,page',
+      date: new Date(),
+      show: true
+    }
+  },
+  methods:{
+    handleClose: function () {
+      this.show = false
+    }
+  },
+  filters: {
+    formatDate: function (value) {
+      var date = new Date(value)
+      var year = date.getFullYear()
+      var month = padDate(date.getMonth() + 1)
+      var day = padDate(date.getDate())
+      var hours = padDate(date.getHours())
+      var minutes = padDate(date.getMinutes())
+      var seconds = padDate(date.getSeconds())
+      return year + '-' + month + '-' + day + '' + hours + ':' + minutes + ':' + seconds;
+    }
+  },
+  mounted () {
+    var _this = this;
+    this.timer = setInterval(function () {
+      _this.date = new Date();
+    },1000);
+  },
+  beforeDestroy () {
+    if(this.timer){
+      clearInterval(this.timer);
     }
   }
+}
+
+var padDate = function (value) {
+  return value < 10 ? '0' + value : value
 }
 </script>
 
--- book_view/src/router/index.js
+++ book_view/src/router/index.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueResource from 'vue-resource'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+Vue.use(VueResource)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'HelloWorld，dygstudio',
       component: HelloWorld
     }
   ]
